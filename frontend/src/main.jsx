9fb2a730e9977e93f7ecaf71bcf6f4192abcd8fb
Abhishek <abhishekkumarcse2015@gmail.com>
2025-01-12 19:27:24 +0530

    Pages setup done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,8 +14,8 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import Signup from "./pages/Signup.js";
-import Login from "./pages/Login.js";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
 // import ForgotPassword from './Pages/ForgotPassword.jsx';
 // import AdminProfile from './Pages/Profile.jsx';
 // import AddHome from './Pages/AddHome.jsx';
@@ -24,25 +24,24 @@ import Login from "./pages/Login.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
+
     <Route path="/" element={<App />}>
       {/* <Route path="" element={<Home />} /> */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/Signup" element={<Signup />} />
-      {/* <Route path="/logout" element={</>} /> */}
-      {/* <Route path="/homes/:id" element={<HomeDetail />} />
-      <Route path="/allpg" element={<AllPg/>} />
+      {/* <Route path="/homes/:id" element={<HomeDetail />} /> */}
+      {/* <Route path="/allpg" element={<AllPg/>} /> */}
       <Route path="/Signup" element={<Signup/>} />
-     <Route path="/login" element={<Login/>} />
-      <Route path="/profile" element={<AdminProfile/>} />
-      <Route path="/AddHome" element={<AddHome/>} />
-      <Route path="/Verifyotp" element={<VerifyOtp/>}/>
-      <Route path="/setpassword" element={<Setpassword/>}/>
+      <Route path="/login" element={<Login/>} />
+      {/* <Route path="/profile" element={<AdminProfile/>} /> */}
+      {/* <Route path="/AddHome" element={<AddHome/>} /> */}
+      {/* <Route path="/Verifyotp" element={<VerifyOtp/>}/> */}
+      {/* <Route path="/setpassword" element={<Setpassword/>}/> */}
 
-      <Route path="/forgotpassword" element={<ForgotPassword/>} /> */}
+      {/* <Route path="/forgotpassword" element={<ForgotPassword/>} /> */}
 
-      {/* <Route path="/logout" element={</>} /> */}
-    </Route>
-  )
+      {/* <Route path="/logout" element={</>} /> */}      
+    </Route>,
+
+  ),
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
